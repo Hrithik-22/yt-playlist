202410f079fe05eae93674ddecf9f8eb4871c046
Hrithik Kedare <kedarehrithik@gmail.com>
2024-05-20 23:36:11 +0530

    comments deleted

--- backend/index.js
+++ backend/index.js
@@ -22,19 +22,6 @@ app.use("/", root);
 
 app.use("/link", link);
 
-//serve static files
-app.use("/", express.static(path.join(__dirname, "/public")));
-
-app.all("*", (req, res) => {
-  res.status(404);
-  if (req.accepts("html")) {
-    res.sendFile(path.join(__dirname, "views", "404.html"));
-  } else if (req.accepts("json")) {
-    res.json({ error: "404 not Found" });
-  } else {
-    res.type("txt").send("404 not Found");
-  }
-});
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
