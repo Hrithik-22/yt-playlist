4d452a4d1c1d071bc0fa2ed05084f88c01d5a995
Hrithik Kedare <kedarehrithik@gmail.com>
2024-05-20 23:20:50 +0530

    index change

--- backend/index.js
+++ backend/index.js
@@ -12,12 +12,28 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 const link = require("./routes/api");
+const root = require("./routes/root");
 
 app.use(cors(corsOptions));
-app.get("/", (req, res) => res.send("Express on Vercel"));
+
+//serve static files
+app.use("/", express.static(path.join(__dirname, "/public")));
+
+//routes
+app.use("/", root);
 
 app.use("/link", link);
 
+app.all("*", (req, res) => {
+  res.status(404);
+  if (req.accepts("html")) {
+    res.sendFile(path.join(__dirname, "views", "404.html"));
+  } else if (req.accepts("json")) {
+    res.json({ error: "404 not Found" });
+  } else {
+    res.type("txt").send("404 not Found");
+  }
+});
 //serve static files
 
 app.listen(PORT, () => {
