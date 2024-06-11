ca261b6ad6bc5e0dfa17baaa32fdb6d733d31e95
Hrithik Kedare <kedarehrithik@gmail.com>
2024-06-11 16:36:25 +0530

    remove-comments

--- backend/routes/api/link.js
+++ backend/routes/api/link.js
@@ -1,8 +1,4 @@
-// import { Router } from "express";
-// import linkController from "../controllers/linkController.js";
-// const router = Router();
-// router.post("/", linkController.index);
-// export default router;
+
 
 const express = require("express");
 const router = express.Router();
@@ -10,7 +6,6 @@ const linkController = require("../../controllers/linkController");
 
 router
   .route("/")
-  // .get(linkController.getLinksByUserId)
   .post(linkController.calculateDuration);
 
 module.exports = router;
